5a4b8bfc7bfebb124639ab614a8e0f31c245cbab
dbond <debon1426@gmail.com>
2020-04-02 02:24:16 -0400

    to be continued

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -1,7 +1,136 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 /**
  * Created by leon on 1/10/18.
  */
-public class SinglyLinkedList {
+public class SinglyLinkedList<T> {
+    Node<T> head = null;
+    Node<T> last = null;
+    int index = 0;
+
+    int nodeCount = 0;
+
+    SinglyLinkedList() {
+
+    }
+
+    public int size() {
+        Node<T> tempNode = head;
+
+        while (tempNode.next != null) {
+
+            tempNode = tempNode.next;
+
+            nodeCount++;
+        }
+        return nodeCount;
+    }
+
+
+    public void add(T value) {
+
+        if (head == null) {
+            Node<T> newNode = new Node<>(null, value);
+
+            head = newNode;
+
+            nodeCount++;
+        } else {
+            Node<T> tempNode = head;
+            while (tempNode.next != null) {
+                tempNode = tempNode.next;
+                nodeCount++;
+            }
+            tempNode.next = new Node<T>(null, value);
+        }}
+
+    public Boolean contains (T value){
+
+        int i = 0;
+
+        Node currentNode = this.head;
+        while (currentNode != null){
+            if (currentNode.getValue().equals(value)){
+                index = 1;
+                break;
+            }
+
+            currentNode = currentNode.getNext();
+            i++;
+        }
+
+        return index == 1;
+    }
+
+    public T getValue(int index){
+        if (index > this.nodeCount){
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node currentNode = this.head;
+        int i = 0;
+        while (currentNode != null){
+            if (i == index) return (T) currentNode.getValue();
+            currentNode = currentNode.getNext();
+            i++;
+        }
+
+        return null;
+    }
+
+    public Integer find (T value){
+
+        if (contains(value)) return index;
+        return -1;
+    }
+
+
+
+
+
+    public class Node<T>{
+
+        Node<T> next;
+        Node<T> prev;
+        T value;
+        int index;
+
+
+        Node (Node<T> next, T value) {
+            this.next=next;
+            this.value= value;
+        }
+
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
+
+
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,94 @@
 package com.zipcodewilmington.singlylinkedlist;
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+
+    @Test
+    public void addTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+       list.add("Destiny");
+       list.add("Kevin");
+
+        assertTrue(list.head.next.value.equals("Kevin"));
+        assertNull(list.head.next.next);
+    }
+
+    @Test
+    public void removeTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add("Destiny");
+        list.add("Kevin");
+
+        assertTrue(list.head.next.value.equals("Destiny"));
+
+    }
+    @Test
+    public void sizeTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add("Destiny");
+        list.add("Kevin");
+        list.size();
+        assertEquals(2,list.size());
+    }
+    @Test
+    public void getTest(){
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add("Destiny");
+        list.add("Kevin");
+
+
+
+    }
+    @Test
+    public void testGetItem(){
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add("First");
+        list.add("Second");
+        list.add("Third");
+
+        String actual = (String) list.getValue(1);
+        String expected = "Second";
+        Assert.assertEquals(expected,actual);
+
+    }
+
+
+    @Test
+    public void contains() {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add("First");
+        list.add("Second");
+        list.add("Third");
+
+        boolean actual = list.contains("Second");
+        Assert.assertTrue(actual);
+
+        list.add(3);
+        list.add(6);
+
+        actual = list.contains(3);
+        Assert.assertTrue(actual);
+    }
+
+    @Test
+    public void find() {
+
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add("First");
+        list.add("Second");
+        list.add("Third");
+
+        Integer actual = list.find("Second");
+        Integer expected = 1;
+        Assert.assertEquals(expected,actual);
+    }
 }
